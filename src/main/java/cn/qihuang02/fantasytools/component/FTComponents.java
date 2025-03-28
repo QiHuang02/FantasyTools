ffd81669a3e561efc59a0f7cb5c2e58496d848da
QiHuang02 <2830447227@qq.com>
2025-03-28 21:23:20 +0800

    修改自定义的DataComponent

--- src/main/java/cn/qihuang02/fantasytools/component/FTComponents.java
+++ src/main/java/cn/qihuang02/fantasytools/component/FTComponents.java
@@ -1,9 +1,11 @@
 package cn.qihuang02.fantasytools.component;
 
 import cn.qihuang02.fantasytools.FantasyTools;
+import com.mojang.serialization.Codec;
 import net.minecraft.core.UUIDUtil;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -15,13 +17,13 @@ public class FTComponents {
     public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENT_TYPES =
             DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, FantasyTools.MODID);
 
-    public static final DeferredHolder<DataComponentType<?>, DataComponentType<UUID>> OWNER_UUID =
-            register("owner_uuid", builder -> builder.persistent(UUIDUtil.CODEC));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<String>> OWNER =
+            register(builder -> builder.persistent(Codec.STRING));
 
     private static <T> DeferredHolder<DataComponentType<?>, DataComponentType<T>> register(
-            String name, UnaryOperator<DataComponentType.Builder<T>> builderOperator
+            UnaryOperator<DataComponentType.Builder<T>> builderOperator
     ) {
-        return DATA_COMPONENT_TYPES.register(name, () -> builderOperator.apply(DataComponentType.builder()).build());
+        return DATA_COMPONENT_TYPES.register("owner", () -> builderOperator.apply(DataComponentType.builder()).build());
     }
 
     public static void register(IEventBus eventBus) {
