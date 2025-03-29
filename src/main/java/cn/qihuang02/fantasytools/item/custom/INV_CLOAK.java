36efee8daab36917729a1ca4816eee52d26313ca
QiHuang02 <2830447227@qq.com>
2025-03-30 00:45:48 +0800

    添加隐身衣

--- src/main/java/cn/qihuang02/fantasytools/item/custom/INV_CLOAK.java
+++ src/main/java/cn/qihuang02/fantasytools/item/custom/INV_CLOAK.java
@@ -1,20 +1,32 @@
 package cn.qihuang02.fantasytools.item.custom;
 
 import cn.qihuang02.fantasytools.item.FTArmorMaterials;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class INV_CLOAK extends ArmorItem {
-    public INV_CLOAK(Properties properties) {
+    public INV_CLOAK(@NotNull Properties properties) {
         super(
                 FTArmorMaterials.INVCLOAK_ARMOR_MATERIAL,
                 Type.CHESTPLATE,
                 properties
                         .stacksTo(1)
                         .rarity(Rarity.valueOf("FANTASYTOOLS_LEGENDARY"))
-                        .durability(-1)
         );
     }
 
-
+    @Override
+    public void appendHoverText(
+            @NotNull ItemStack stack,
+            @NotNull TooltipContext context,
+            @NotNull List<Component> tooltipComponents,
+            @NotNull TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+    }
 }
