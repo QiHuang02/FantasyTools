48b51d681af76ad0c7443354da10dad414dbffdb
QiHuang02 <2830447227@qq.com>
2025-03-30 00:45:33 +0800

    添加自定义盔甲材料

--- src/main/java/cn/qihuang02/fantasytools/item/FTArmorMaterials.java
+++ src/main/java/cn/qihuang02/fantasytools/item/FTArmorMaterials.java
@@ -21,8 +21,8 @@ import java.util.function.Supplier;
 public class FTArmorMaterials {
     public static final Holder<ArmorMaterial> INVCLOAK_ARMOR_MATERIAL = register("invisible_fur",
             Util.make(new EnumMap<>(ArmorItem.Type.class), attribute -> {
-                attribute.put(ArmorItem.Type.CHESTPLATE, 2);
-            }), 30, 2f, 0, FTItems.DEMIGUISE_FUR);
+                attribute.put(ArmorItem.Type.CHESTPLATE, 4);
+            }), 30, 4f, 0, FTItems.DEMIGUISE_FUR);
 
     private static @NotNull Holder<ArmorMaterial> register(String name,
                                                            EnumMap<ArmorItem.Type, Integer> typeProtection,
